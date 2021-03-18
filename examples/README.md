1cec25ca303a608ef7865657a466435b537ca74e
bekzatalish <bekzatalish@gmail.com>
2021-03-17 23:16:52 -0400

    Created gh-pages

--- examples/README.md
+++
@@ -1,3 +0,0 @@
-This folder contains examples.
-
-Ensure that `zeroml` is installed before running the example scripts.
--- zeroml/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.0.1"
-__url__ = "https://github.com/bekzatalish/zero-ml"
-__description__ = "Machine learning algorithms implemented from scratch using Python and NumPy."
-__author__ = "Bekzat Alish"
-__email__ = "bekzatalish@gmail.com"
